18da655a61935532e7da07346524cc52c7d537eb
cielSwift <101886648@qq.com>
2020-04-16 16:41:27 +0800

    测试

--- scat-quick/src/main/java/com/ciel/scatquick/anntion/SpiClass.java
+++ scat-quick/src/main/java/com/ciel/scatquick/anntion/SpiClass.java
@@ -13,13 +13,4 @@ public class SpiClass implements SpiInterface {
         return in+">>";
     }
 
-    public static void main(String[] args) {
-
-        ServiceLoader<SpiInterface> serviceLoader = ServiceLoader.load(SpiInterface.class);
-
-        serviceLoader.forEach(t -> {
-            t.vue("a");
-        });
-
-    }
 }
