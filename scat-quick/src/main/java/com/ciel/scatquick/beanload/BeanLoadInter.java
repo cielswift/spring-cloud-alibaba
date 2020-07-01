ac3cf3b5e3d86e8b02c620577e2a735aee233822
xiapeixin <1018866480@qq.com>
2020-07-01 16:51:08 +0800

    info

--- scat-quick/src/main/java/com/ciel/scatquick/beanload/BeanLoadInter.java
+++ scat-quick/src/main/java/com/ciel/scatquick/beanload/BeanLoadInter.java
@@ -11,6 +11,14 @@ import org.springframework.stereotype.Component;
  *   getBean -> doGetBean -> createBean  已创建的bean 缓存到singletonObject (Map中)
  *  尝试给bean 创建代理bean
  *  org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory#createBean(..) resolveBeforeInstantiation
+ *
+ *  在Spring的DefaultSingletonBeanRegistry类中，你会赫然发现类上方挂着这三个Map：
+ * singletonObjects 它是我们最熟悉的朋友，俗称“单例池”“容器”，缓存创建完成单例Bean的地方。
+ *
+ * singletonFactories 映射创建Bean的原始工厂
+ *
+ * earlySingletonObjects 映射Bean的早期引用，也就是说在这个Map里的Bean不是完整的，甚至还不能称之为“Bean”，只是一个Instance.
+ *
  */
 @Component
 public class BeanLoadInter  implements BeanPostProcessor  {
